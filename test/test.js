e46d8bdb9f8de1fb2b002aa94824cdf63c2a9324
式燕 <wenjian.ywj@alibaba-inc.com>
2017-09-11 20:50:55 +0800

    init

---
+++ test/test.js
@@ -0,0 +1,23 @@
+const css = require('css')
+const assert = require('assert')
+const path = require('path')
+const fs = require('fs')
+const stringify = require('../lib/stringify')
+
+describe('css stringify', function () {
+  it('可以正确的 stringify 文件', function () {
+    const a = require('./source/a.json')
+    assert(
+      stringify(a),
+      fs.readFileSync(path.join(__dirname, './expect/a.css'), 'utf-8')
+    )
+  })
+
+  it('可以使用 indent', function () {
+    const indent = require('./source/indent.json')
+    assert(
+      stringify(indent, 4),
+      fs.readFileSync(path.join(__dirname, './expect/indent.css'), 'utf-8')
+    )
+  })
+})
