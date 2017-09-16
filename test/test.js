576da585e55ff835ce670b07c83c3654f1b7a004
式燕 <wenjian.ywj@alibaba-inc.com>
2017-09-16 22:08:22 +0800

    fixed bug

--- test/test.js
+++ test/test.js
@@ -3,10 +3,11 @@ const path = require('path')
 const fs = require('fs')
 const stringify = require('../lib/stringify')
 
+
 describe('css stringify', function () {
   it('可以正确的 stringify 文件', function () {
     const a = require('./source/a.json')
-    assert(
+    assert.equal(
       stringify(a),
       fs.readFileSync(path.join(__dirname, './expect/a.css'), 'utf-8')
     )
@@ -14,7 +15,7 @@ describe('css stringify', function () {
 
   it('可以使用 indent', function () {
     const indent = require('./source/indent.json')
-    assert(
+    assert.equal(
       stringify(indent, 4),
       fs.readFileSync(path.join(__dirname, './expect/indent.css'), 'utf-8')
     )
