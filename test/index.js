048dfe0a0afb644106050d8925dc5180334fa536
JackXie60 <1809697652@qq.com>
2022-10-22 12:05:42 +0800

    css的ast解析

---
+++ test/index.js
@@ -0,0 +1,34 @@
+const assert = require('assert')
+const path = require('path')
+const fs = require('fs')
+const stringify = require('../lib/stringify')
+
+const a = require('./source/a.json')
+// assert.equal(
+//     stringify(a),
+//     fs.readFileSync(path.join(__dirname, './expect/a.css'), 'utf-8')
+// )
+console.log(stringify(a))
+// const indent = require('./source/indent.json')
+// assert.equal(
+//     stringify(indent, 4),
+//     fs.readFileSync(path.join(__dirname, './expect/indent.css'), 'utf-8')
+// )
+
+// describe('css stringify', function () {
+//     it('可以正确的 stringify 文件', function () {
+//         const a = require('./source/a.json')
+//         assert.equal(
+//             stringify(a),
+//             fs.readFileSync(path.join(__dirname, './expect/a.css'), 'utf-8')
+//         )
+//     })
+
+//     it('可以使用 indent', function () {
+//         const indent = require('./source/indent.json')
+//         assert.equal(
+//             stringify(indent, 4),
+//             fs.readFileSync(path.join(__dirname, './expect/indent.css'), 'utf-8')
+//         )
+//     })
+// })
--- test/test.js
+++ test/test.js
@@ -4,20 +4,20 @@ const fs = require('fs')
 const stringify = require('../lib/stringify')
 
 
-describe('css stringify', function () {
-  it('可以正确的 stringify 文件', function () {
-    const a = require('./source/a.json')
-    assert.equal(
-      stringify(a),
-      fs.readFileSync(path.join(__dirname, './expect/a.css'), 'utf-8')
-    )
-  })
+// describe('css stringify', function () {
+//   it('可以正确的 stringify 文件', function () {
+//     const a = require('./source/a.json')
+//     assert.equal(
+//       stringify(a),
+//       fs.readFileSync(path.join(__dirname, './expect/a.css'), 'utf-8')
+//     )
+//   })
 
-  it('可以使用 indent', function () {
-    const indent = require('./source/indent.json')
-    assert.equal(
-      stringify(indent, 4),
-      fs.readFileSync(path.join(__dirname, './expect/indent.css'), 'utf-8')
-    )
-  })
-})
+//   it('可以使用 indent', function () {
+//     const indent = require('./source/indent.json')
+//     assert.equal(
+//       stringify(indent, 4),
+//       fs.readFileSync(path.join(__dirname, './expect/indent.css'), 'utf-8')
+//     )
+//   })
+// })
